97a5db0c6c393ead857552d35d4ba1551b01b44e
xkdvd <100168196+xkdvd@users.noreply.github.com>
2023-06-11 16:22:46 -0700

    Update smtp_client.py

--- smtp_client.py
+++ smtp_client.py
@@ -8,7 +8,7 @@ msg['To'] = email.utils.formataddr(('Recipient', 'recipient@example.com'))
 msg['From'] = email.utils.formataddr(('Author', 'author@example.com'))
 msg['Subject'] = 'Simple test message'
 
-server = smtplib.SMTP('127.0.0.1', 80)
+server = smtplib.SMTP('127.0.0.1', 25)
 server.set_debuglevel(True) # show communication with the server
 try:
     server.sendmail('author@example.com', ['recipient@example.com'], msg.as_string())
